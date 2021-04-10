e138163c61b7a09ea5f8a044168a285ed88ea3dd
junlee <laonzenamoon@gmail.com>
2021-04-10 17:24:42 +0900

    refactor: 기타 리팩토링

--- eureka-server/src/main/java/me/lozm/global/config/ApplicationConfig.java
+++ eureka-server/src/main/java/me/lozm/global/config/ApplicationConfig.java
@@ -1,4 +1,4 @@
-package lozm.eureka.global.config;
+package me.lozm.global.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
